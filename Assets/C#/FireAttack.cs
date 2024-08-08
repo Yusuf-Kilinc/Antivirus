d1c8a86984ddb06eb7f60659955941e6f4838fbf
Yusuf-Kilinc <104257668+Yusuf-Kilinc@users.noreply.github.com>
2024-08-08 14:37:03 +0300

    Data

---
+++ Assets/C#/FireAttack.cs
@@ -0,0 +1,71 @@
+using TMPro;
+using UnityEngine;
+
+public class FireAttack : MonoBehaviour
+{
+
+    public float Mermi, Sarjor, FullBullet, Menzil, Hasar1, Hasar2, NewBullet, FireTime, sayi, Zmn, MaxZmn;
+    public bool Fire, Reload;
+    RaycastHit hit;
+    public TextMeshProUGUI Bullettext;
+    float asasas;
+    private void Start()
+    {
+        Zmn = MaxZmn;
+    }
+
+
+    void Update()
+    {
+        Bullettext.text = "" + Mermi + "/" + FullBullet;
+
+        if (Input.GetMouseButton(0) && Mermi > 0 && Time.time > NewBullet && !Reload) 
+        {
+            Fire = true;
+            NewBullet = Time.time + FireTime;
+            Mermi--;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Reload = true;
+        }
+
+        if (Reload == true && Mermi != 30 && !Reload)  
+        {
+            sayi = Sarjor - Mermi;
+            Zmn -= Time.deltaTime;
+            if (Zmn <= 0)
+            {
+                Reload = false;
+                Zmn = MaxZmn;
+                if (sayi > FullBullet)
+                {
+                    Mermi += FullBullet;
+                    FullBullet = 0;
+
+                }
+                if (sayi < FullBullet)
+                {
+                    Mermi += sayi;
+                    FullBullet -= sayi;
+                }
+             
+            }
+           
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (Fire)
+        {
+            Fire = false;
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Menzil))
+            {
+                if (hit.transform.tag == "Enemy")
+                {
+                    Debug.Log("Dusman Temas");
+                }
+            }
+        }
+    }
+}
