e4eafd50dfa9266431f09936229e2986f3e440bb
Yusuf-Kilinc <104257668+Yusuf-Kilinc@users.noreply.github.com>
2024-08-12 12:23:59 +0300

    Data
    Data

--- Assets/C#/FireAttack.cs
+++ Assets/C#/FireAttack.cs
@@ -1,13 +1,27 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FireAttack : MonoBehaviour
 {
 
     public float Mermi, Sarjor, FullBullet, Menzil, Hasar1, Hasar2, NewBullet, FireTime, sayi, Zmn, MaxZmn;
+    public float RedSayi, RedMer, RedFull, newRed, redSarjor;
     public bool Fire, Reload;
+    public bool RedTrue, RedReload;
+
     RaycastHit hit;
     public TextMeshProUGUI Bullettext;
+    public ParticleSystem muzzleFlash;
+    public AudioSource ses;
+    public AudioClip sound;
+    public Image Cursor;
+    private string currentAmmoColor = "Red"; // Varsayýlan renk
+    public GameObject DeathObj;
+    float Mesafe = 50;
+    GameObject Hedef;
+
     float asdasdasdas;
     private void Start()
     {
@@ -15,7 +29,52 @@ public class FireAttack : MonoBehaviour
     }
     void Update()
     {
-        Bullettext.text = "" + Mermi + "/" + FullBullet;
+        #region CursorSystem
+        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * Mesafe, Color.yellow);
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mesafe))
+        {
+            if (hit.transform.CompareTag("Enemy"))
+            {
+                GameObject Chedef = hit.transform.gameObject;
+                Target CtargetScript = Chedef.GetComponent<Target>();
+                if (CtargetScript != null)
+                {
+                    if (CtargetScript.enemyColor == "Red")
+                    {
+                        Cursor.color = Color.red;
+                    }
+                    else if (CtargetScript.enemyColor == "Green")
+                    {
+                        Cursor.color = Color.green;
+                    }
+                    else if (CtargetScript.enemyColor == "Blue")
+                    {
+                        Cursor.color = Color.blue;
+                    }
+                    else
+                    {
+                        Cursor.color = Color.white;
+                    }
+                }
+            }
+            else
+            {
+                Cursor.color = Color.white;
+            }
+        }
+        else
+        {
+            Cursor.color = Color.white;
+        }
+
+
+        #endregion
+
+        if (!RedTrue)
+        {
+            Bullettext.text = "" + Mermi + "/" + FullBullet;
+        }
 
         if (Input.GetMouseButton(0) && Mermi > 0 && Time.time > NewBullet && !Reload)
         {
@@ -27,11 +86,11 @@ public class FireAttack : MonoBehaviour
         {
             Reload = true;
         }
-        if (Reload == true && Mermi == 30)
+        if (Reload == true && Mermi != 30)
         {
             Zmn -= Time.deltaTime;
             sayi = Sarjor - Mermi;
-           
+
             if (Zmn <= 0)
             {
                 Reload = false;
@@ -49,6 +108,50 @@ public class FireAttack : MonoBehaviour
                 }
             }
         }
+
+        if (RedTrue)
+        {
+            Bullettext.text = "" + RedMer + "/" + RedFull;
+            if (Input.GetMouseButton(0) && RedMer > 0 && Time.time > newRed && !Reload)
+            {
+                Fire = true;
+                newRed = Time.time + FireTime;
+                RedMer--;
+                muzzleFlash.Play();
+                ses.clip = sound;
+                ses.Play();
+            }
+            else
+            {
+                muzzleFlash.Stop();
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                RedReload = true;
+            }
+            if (RedReload == true && RedMer != 30)
+            {
+                Zmn -= Time.deltaTime;
+                RedSayi = redSarjor - RedMer;
+
+                if (Zmn <= 0)
+                {
+                    RedReload = false;
+                    Zmn = MaxZmn;
+                    if (RedSayi > RedFull)
+                    {
+                        RedMer += RedFull;
+                        RedFull = 0;
+
+                    }
+                    if (RedSayi < RedFull)
+                    {
+                        RedMer += RedSayi;
+                        RedFull -= RedSayi;
+                    }
+                }
+            }
+        }
     }
     private void FixedUpdate()
     {
@@ -57,11 +160,44 @@ public class FireAttack : MonoBehaviour
             Fire = false;
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Menzil))
             {
-                if (hit.transform.tag == "Enemy")
+                if (hit.transform.CompareTag("Enemy"))
                 {
-                    Debug.Log("Dusman Temas");
+                    Hedef = hit.transform.gameObject;
+                    Target targetScript = Hedef.GetComponent<Target>();
+
+                    if (targetScript != null)
+                    {
+                        if (currentAmmoColor == "Red" && targetScript.enemyColor == "Red")
+                        {
+                            targetScript.TakeDamage(100);
+                        }
+                        else if (currentAmmoColor == "Green" && targetScript.enemyColor == "Green")
+                        {
+                            targetScript.TakeDamage(100);
+                        }
+                        else if (currentAmmoColor == "Blue" && targetScript.enemyColor == "Blue")
+                        {
+                            targetScript.TakeDamage(100);
+                        }
+                        else
+                        {
+                            targetScript.GainHealth(10);
+                        }
+                    }
+                    Instantiate(DeathObj, hit.point, Quaternion.identity);
+                    StartCoroutine(YOKET());
+                    Debug.Log("Raycast isabet etti: " + hit.transform.name);
                 }
             }
         }
+    }
+    IEnumerator YOKET()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(Hedef);
+    }
+    public void RedBullet()
+    {
+        RedTrue = true;
     }
 }
